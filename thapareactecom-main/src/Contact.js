747fe9b2c3a44f6f3ee9926b7281ace949bcf904
niladri2002 <niladri.sadhu2002@gmail.com>
2024-04-08 11:25:20 +0530

    Contact.js

--- thapareactecom-main/src/Contact.js
+++ thapareactecom-main/src/Contact.js
@@ -29,6 +29,13 @@ const Contact = () => {
             }
           }
         }
+
+        /* Add this block to convert text to lowercase */
+        input[type="text"],
+        input[type="email"],
+        textarea {
+          text-transform: lowercase;
+        }
       }
     }
   `;
@@ -38,23 +45,25 @@ const Contact = () => {
       <h2 className="common-heading">Contact page</h2>
 
       <iframe
-         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3673.787578325883!2d88.43280867508979!3d22.95804897921919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f894e35e8c5f25%3A0xdd507c3dfb01c004!2sIIIT%20Kalyani%20Rd%2C%20Block%20B%2C%20Kalyani%2C%20Kanchrapara%2C%20West%20Bengal%20741235!5e0!3m2!1sen!2sin!4v1710058016231!5m2!1sen!2sin" 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3673.787578325883!2d88.43280867508979!3d22.95804897921919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f894e35e8c5f25%3A0xdd507c3dfb01c004!2sIIIT%20Kalyani%20Rd%2C%20Block%20B%2C%20Kalyani%2C%20Kanchrapara%2C%20West%20Bengal%20741235!5e0!3m2!1sen!2sin!4v1710058016231!5m2!1sen!2sin"
         width="100%"
         height="400"
         style={{ border: 0 }}
         allowFullScreen=""
         loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"></iframe>
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
 
       <div className="container">
         <div className="contact-form">
           <form
             action="https://formspree.io/f/xpzvllbp"
             method="POST"
-            className="contact-inputs">
+            className="contact-inputs"
+          >
             <input
               type="text"
-              placeholder="username"
+              placeholder="Username"
               name="username"
               required
               autoComplete="off"
@@ -62,7 +71,7 @@ const Contact = () => {
 
             <input
               type="email"
-              name="Email"
+              name="email"
               placeholder="Your email"
               autoComplete="off"
               required
@@ -70,21 +79,22 @@ const Contact = () => {
 
             <input
               type="email"
-              name="Email"
+              name="shopkeeperEmail"
               placeholder="Shopkeeper's email"
               autoComplete="off"
               required
             />
 
             <textarea
-              name="Message"
+              name="message"
               cols="30"
               rows="10"
               required
               autoComplete="off"
-              placeholder="Enter you message"></textarea>
+              placeholder="Enter your message"
+            ></textarea>
 
-            <input type="submit" value="send" />
+            <input type="submit" value="Send" />
           </form>
         </div>
       </div>
